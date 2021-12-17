e6fdc898900f9473e92d4fc9ae7b90a839e815b6
taauntik <devauntik@gmail.com>
2021-12-17 21:41:26 +0600

    learned about router in express

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
-const express = require('express');
-const adminRouter = require('./adminRouter');
-const publicRouter = require('./publicRouter');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
+const UPLOADS_FOLDER = "./uploads/";
+
+// define a storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOADS_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    // important file.pdf
+    const fileExt = path.extname(file.originalname);
+    const filename =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now();
+    cb(null, filename + fileExt);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.fieldname === "avatar") {
+      if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+      ) {
+        cb(null, true);
+      } else {
+        cb(new Error("Only .jpg .jpeg or .png format allowed!"));
+      }
+    } else if (file.fieldname === "doc") {
+      if (file.mimetype === "application/pdf") {
+        cb(null, true);
+      } else {
+        cb(new Error("Only .pdf format allowed"));
+      }
+    } else {
+      cb(new Error("There was an unknown error"));
+    }
+  },
+});
 const app = express();
 
-app.use('/admin', adminRouter);
-app.use('/', publicRouter);
+// application route
+app.post(
+  "/",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "doc", maxCount: 1 },
+  ]),
+  (req, res) => {
+    console.log(req.files);
+    res.send("Hello world");
+  }
+);
 
-app.listen(3000, () => console.log('listening to port 3000'));
+// default error Handler
+
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).send("there was an upload error!");
+    }
+    res.status(500).send(err.message);
+  } else {
+    res.send("success");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
